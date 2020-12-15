6e7b3af48ca667da82be9b3b9abd59dac770aebe
Arhelf1 <Arhelf1@gmail.com>
2020-12-15 22:04:13 +0300

    Update test.pl
    Немного поменял переменные, потому что слишком громоздко

--- test.pl
+++ test.pl
@@ -2,14 +2,15 @@ maxim(X,Y,X):- X >= Y,!.
 maxim(_,Y,Y).
 maxim(X,Y,X,Z,_,Z):- X >= Y,!.  
 maxim(_,Y,Y,_,Q,Q).
-pred_per(D,0,Five_two, D) :- Not_Nool is D mod Five_two, Not_Nool \= 0, !.
-pred_per(D, How_much, Five_two, New_D):- D1 is D div Five_two,
-	pred_per(D1, How_much1, Five_two, New_D), How_much is How_much1+1.
+pred_per(D,0,F_T, D) :- NN is D mod FT, NN \= 0, !.
+pred_per(D, How_much, FT, New_D):- D1 is D div FT,
+	pred_per(D1, How_much1, FT, New_D), How_much is How_much1+1.
 per(1, -1, _, _).
-per(D, How_much, Ostatok, Ostatok__) :- Not_Nool is Ostatok mod D, Not_Nool \= 0,
-  Ostatok1 is Not_Nool, Ostatok2 is Ostatok1*10, Ostatok3 is Ostatok2+9, per(D, How_much1, Ostatok3, Ostatok__),
+per(D, How_much, Ost, Ost__) :- NN is Ost mod D, NN \= 0,
+  Ost1 is NN, Ost2 is Ost1*10, Ost3 is Ost2+9, per(D, How_much1, Ost3, Ost__),
   How_much is How_much1+1.
-per(_, 0, Ostatok, Ostatok) :- !.
+per(_, 0, Ost, Ost) :- !.
 len(1,0) :- !.
-len(D, Length_of_per):- pred_per(D, _, 2, New_D), pred_per(New_D, _, 5, New_D1), 
-  per(New_D1, How_much, 9, _), Length_of_per is How_much+1,!.
+len(D, Len_per):- pred_per(D, _, 2, New_D), pred_per(New_D, _, 5, New_D1), 
+  per(New_D1, How_much, 9, _), Len_per is How_much+1,!.
+ 
